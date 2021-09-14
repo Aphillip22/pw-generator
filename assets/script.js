5bef0d1726e4722cf5ca60fcf271b0b4ab625950
Aphillip22 <amber.rain.phillips@gmail.com>
2021-09-13 19:13:29 -0600

    correct code

--- assets/script.js
+++ assets/script.js
@@ -1,67 +1,75 @@
 // password criteria functions begin
-var pwLength = function() {
-    // ask if password should contain both upper and lower case
-    var promptLength = window.Number(prompt("How many characters should this password contain? Please choose a number between 8 and 128."));
-         // convert answer from prompt to an actual number
-        promptLength = parseInt(promptLength);
-        // validate prompt answer
-        if (promptLength === "" || promptLength === null || promptLength < 8 || promptLength > 128) {
-            window.alert("You need to provide a valid answer! Please try again.");
-            // use return to call it again and stop the rest of this function from running
-            return pwLength();
-        }
-        else {
-            pwType();
-        }
-}
-  var pwType = function() {
-    //ask for character type
-    var promptType = window.prompt("Enter a character type: special, numeric, uppercase, lowercase, all.");
-    // convert promptType to all lowercase
-    var promptType = promptType.toLowerCase();
-    // validate prompt answer
-    if (promptType === "" || promptType === null) {
-      window.alert("You need to provide a valid answer! Please try again.");
-      // use return to call it again and stop the rest of this function from running
-      return charType();
-    }
-    else {
-      writePassword();
-    }
+var pwLength = function () {
+  // ask if password should contain both upper and lower case
+  var promptLength = window.Number(prompt("How many characters should this password contain? Please choose a number between 8 and 128."));
+  // convert answer from prompt to an actual number
+  promptLength = parseInt(promptLength);
+  // validate prompt answer
+  if (promptLength === "" || promptLength === null || promptLength < 8 || promptLength > 128) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    // use return to call it again and stop the rest of this function from running
+    return pwLength();
   }
-  
-  // Get references to the #generate element
-  var generateBtn = document.querySelector("#generate");
-  
-  // Write password to the #password input
+  else {
+    return promptLength;
+  }
+}
 
-  function writePassword() {
-    var passwordText = document.querySelector("#password");
-    passwordText.value = retVal;
-    //evaluate character type
-    var charSet = "";
-    if(pwType === "lowercase" ) {
-      charSet = "abcdefghijklmnopqrstuvwxyz";
-    } else if(pwType === "uppercase" ) {
-      charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    } else if(pwType === "numeric" ) {
-      charSet = "0123456789";
-    } else if(pwType === "special" ) {
-      charSet = "!$%&'()*+,-./:;<=>?@[]^_`{|}~";
-    } else if(pwType === "all") {
-      charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%&'()*+,-./:;<=>?@[]^_`{|}~"
-    }
-    console.log(charSet);
-    
-    //return value
-    var retVal = "";
-    for (var i = 0; i < pwLength; i++) {
-      //picks a character within charSet at index of random number
-      retVal += charSet.charAt(Math.random() * charSet.length);
-    }
-    console.log(retVal);
-    return retVal
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+
+function writePassword() {
+  var passwordText = document.querySelector("#password");
+
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var numeric = "0123456789";
+
+  var special = "!$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  var passwordLength = pwLength();
+  
+  var isLowerCase = confirm("Does your password need lowercase characters?")
+
+  var isUpperCase = confirm("Does your password need uppercase characters?")
+
+  var isNumeric = confirm("Does your password need numeric characters?")
+
+  var isSpecial = confirm("Does your password need special characters?")
+
+  var charSet = "" 
+
+  if (isLowerCase) {
+    charSet += lowerCase;
+  }
+
+  if (isUpperCase) {
+    charSet += upperCase;
+  }
+
+  if (isNumeric) {
+    charSet += numeric;
+  }
+
+  if (isSpecial) {
+    charSet += special;
+  }
+  if (!isLowerCase && !isUpperCase && !isNumeric && !isSpecial) {
+    alert("No character types were selected, please select a minimum of one character type.")
+    return writePassword();
+  }
+  var retVal = "";
+  for (var i = 0; i < passwordLength; i++) {
+    //picks a character within charSet at index of random number
+    retVal += charSet.charAt(Math.random() * charSet.length);
   }
  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", pwLength);
+  passwordText.value = retVal;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
